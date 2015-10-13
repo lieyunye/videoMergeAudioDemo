423e8219f527c47d18b9f0dab36cadf0308aa7aa
lieyunye <lieyunye@gmail.com>
2015-10-13 20:44:56 +0800

    init

---
+++ videoMergeAudio/ViewController.m
@@ -0,0 +1,73 @@
+//
+//  ViewController.m
+//  videoMergeAudio
+//
+//  Created by lieyunye on 10/13/15.
+//  Copyright © 2015 lieyunye. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *audioPath = [[NSBundle mainBundle] pathForResource:@"1" ofType:@"mp4"];
+    NSURL *audioURL = [NSURL fileURLWithPath:audioPath];
+    
+    NSString *videoPath = [[NSBundle mainBundle] pathForResource:@"2" ofType:@"mp4"];
+    NSURL *videoURL = [NSURL fileURLWithPath:videoPath];
+
+    
+    AVURLAsset* audioAsset = [[AVURLAsset alloc]initWithURL:audioURL options:nil];
+    AVURLAsset* videoAsset = [[AVURLAsset alloc]initWithURL:videoURL options:nil];
+    
+    AVMutableComposition* mixComposition = [AVMutableComposition composition];
+    
+    AVMutableCompositionTrack *compositionCommentaryTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio
+                                                                                        preferredTrackID:kCMPersistentTrackID_Invalid];
+    [compositionCommentaryTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, audioAsset.duration)
+                                        ofTrack:[[audioAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0]
+                                         atTime:kCMTimeZero error:nil];
+    
+    AVMutableCompositionTrack *compositionVideoTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo
+                                                                                   preferredTrackID:kCMPersistentTrackID_Invalid];
+    [compositionVideoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoAsset.duration)
+                                   ofTrack:[[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0]
+                                    atTime:kCMTimeZero error:nil];
+    
+    AVAssetExportSession* _assetExport = [[AVAssetExportSession alloc] initWithAsset:mixComposition
+                                                                          presetName:AVAssetExportPresetPassthrough];
+    NSString* videoName = @"export.mp4";
+    
+    NSString *exportPath = [NSTemporaryDirectory() stringByAppendingPathComponent:videoName];
+    NSURL *exportUrl = [NSURL fileURLWithPath:exportPath];
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:exportPath]){
+        [[NSFileManager defaultManager] removeItemAtPath:exportPath error:nil];
+    }
+    
+    _assetExport.outputFileType = @"public.mpeg-4";
+    NSLog(@"file type %@",_assetExport.outputFileType);
+    _assetExport.outputURL = exportUrl;
+    _assetExport.shouldOptimizeForNetworkUse = YES;
+    
+    [_assetExport exportAsynchronouslyWithCompletionHandler:^(void ) {
+         // your completion code here  
+     }
+     ];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
